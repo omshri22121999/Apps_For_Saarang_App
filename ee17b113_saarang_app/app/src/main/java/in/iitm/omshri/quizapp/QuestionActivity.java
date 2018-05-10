f879613d301b88143311660d32f945e58cb0fedb
omshri22121999 <omshriprasath@gmail.com>
2018-05-10 11:46:21 +0530

    New-commit

---
+++ ee17b113_saarang_app/app/src/main/java/in/iitm/omshri/quizapp/QuestionActivity.java
@@ -0,0 +1,23 @@
+package in.iitm.omshri.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class QuestionActivity extends AppCompatActivity {
+    TextView txt_ques_no;
+    TextView txt_ques;
+    RadioButton rad_opt_one;
+    RadioButton rad_opt_two;
+    RadioButton rad_opt_three;
+    RadioButton rad_opt_four;
+    Button  btn_submit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+
+    }
+}
