7fa3b50e71cfc00c3bdf035ed3cc3e555eff7eed
omshri22121999 <omshriprasath@gmail.com>
2018-05-13 22:36:11 +0530

    Finished memes: l

--- ee17b113_saarang_app_memes/app/src/main/java/com/example/omshri/memesapp/Activities/MemesActivity.java
+++ ee17b113_saarang_app_memes/app/src/main/java/com/example/omshri/memesapp/Activities/MemesActivity.java
@@ -1,5 +1,10 @@
 package com.example.omshri.memesapp.Activities;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -31,8 +36,32 @@ public class MemesActivity extends AppCompatActivity {
         memesList = gson.fromJson(memesJSON,MemesList.class);
         memeRecycler = (RecyclerView) findViewById(R.id.memerecycler);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        memeRecycler.setLayoutManager(mLayoutManager);MemesAdapter memesAdapter = new MemesAdapter(memesList.getMemes());
+        memeRecycler.setLayoutManager(mLayoutManager);MemesAdapter memesAdapter = new MemesAdapter(memesList.getMemes(),getApplicationContext());
         memeRecycler.setAdapter(memesAdapter);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        2);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
         //name.setText(memesList.getName(1));
         /*Picasso.get()
              .load(memesList.getURL(1))
